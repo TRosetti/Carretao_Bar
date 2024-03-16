6ecd8fb8f951770b6a6715622909dfb04e185c22
TRosetti <105075815+TRosetti@users.noreply.github.com>
2024-03-16 18:12:45 -0300

    Promoção

--- js/changeMenu.js
+++ js/changeMenu.js
@@ -121,7 +121,8 @@ function changeMenu() {
     
     function sab(){
         h4_alerta.innerHTML = 'Promoção do Dia'
-        my_1.innerHTML = 'Hoje temos <span>Ancho a R$89,00</span>, ja peça o seu!'
+        // my_1.innerHTML = 'Hoje temos <span>Ancho a R$89,00</span>, ja peça o seu!'
+        my_1.innerHTML = 'Hoje estamos sem promoção'
         const promocoes = document.querySelector('#promocoes > div.card_')
 
         priceRegular.forEach((price) =>{
@@ -135,7 +136,8 @@ function changeMenu() {
         if(promocoes != null){
             promocoes.innerHTML = ''
             // promocoes.innerHTML = '<h2 class="card_title"><span>Drink em Dobro</span></h2>'
-            promocoes.innerHTML = '<h2 class="card_title"><span>Ancho a R$89,00</span></h2>'
+            // promocoes.innerHTML = '<h2 class="card_title"><span>Ancho a R$89,00</span></h2>'
+            promocoes.innerHTML = 'Hoje estamos sem promoção'
         }
         normalPricePopup.classList.remove('disabled')
         descountPricePopup.style.display = 'none'
